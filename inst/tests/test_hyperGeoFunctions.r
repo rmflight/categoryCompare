72f7610e6d6fb8f9c8ddb57069a8a0fffa73d003
Robert M Flight <r.flight79@gmail.com>
2012-07-16 16:36:32 -0400

    added hyperGParamsCC class creation tests

--- inst/tests/test_hyperGeoFunctions.r
+++ inst/tests/test_hyperGeoFunctions.r
@@ -5,9 +5,22 @@ require(GO.db)
 dataEnv <- new.env()
 data(ccData, package="categoryCompare", envir=dataEnv)
 g10 <- unique(dataEnv$table10$Entrez)
-testGO <- new("GOHyperGParamsCC", geneIds=g10, universeGeneIds=dataEnv$gUniverse, 
-							annotation="org.Hs.eg.db", ontology="CC", conditional=FALSE, 
-							testDirection="over",fdr=0, pvalueCutoff = 0.05)
+
+test_that("can create new HyperGParamsCC objects", {
+	testGO <- new("GOHyperGParamsCC", geneIds=g10, universeGeneIds=dataEnv$gUniverse, 
+								annotation="org.Hs.eg.db", ontology="CC", conditional=FALSE, 
+								testDirection="over",fdr=0, pvalueCutoff = 0.05)
+	expect_that(testGO, is_equivalent_to(dataEnv$hyperGParamT))
+	testKEGG <- new("KEGGHyperGParamsCC", geneIds=g10, universeGeneIds=dataEnv$gUniverse,
+									annotation="org.Hs.eg.db", testDirection="over",
+									fdr=0, pvalueCutoff=0.05)
+	expect_that(testKEGG, is_a("KEGGHyperGParamsCC"))
+	
+	expect_that(new("GOHyperGParams", geneIds=g10, universeGeneIds=dataEnv$gUniverse, 
+									annotation="org.Hs.eg.db", ontology="CC", conditional=FALSE, 
+									testDirection="over",fdr=0), throws_error())
+	
+})
 
 test_that("can calculate basic hypergeometric enrichment", {
 	resultGO <- hyperGTestCC(testGO)
