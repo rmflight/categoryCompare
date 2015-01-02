16ab5ad9384ed75772d51a155ec2491437a924dd
Robert Flight <r.flight79@gmail.com>
2015-01-02 10:06:31 -0500

    added annotation class

--- R/all_classes.R
+++ R/all_classes.R
@@ -11,3 +11,24 @@ enriched_result <- setClass("enriched_result",
                                          universe = "ANY",
                                          annotation = "list",
                                          statistics = "list"))
+
+#' annotation class
+#' 
+#' This class holds an annotation object that defines how annotations relate to
+#' features, as well as various pieces about each annotation
+#' 
+#' These objects may be created by hand, or may result from specific functions
+#' to create them. Most notably, this package provides functions for creating
+#' a Gene Ontology annotation.
+#' 
+#' @slot annotation_features list of annotation to feature relationships
+#' @slot counts numeric vector of how many features are in each annotation
+#' @slot links character vector defining html links for each annotation (may be empty)
+#' @slot type a one word short description of the "type" of annotation
+#' 
+#' @export
+annotation <- setClass("annotation",
+                       slots = list(annotation_features = "list",
+                                    counts = "numeric",
+                                    links = "character",
+                                    type = "character"))
