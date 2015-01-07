89fe61508e712306adb990c487d2bdbe8e1a0ea6
Robert Flight <r.flight79@gmail.com>
2015-01-07 13:14:07 -0500

    starting to structure the function for querying lists

---
+++ R/significant_results.R
@@ -0,0 +1,17 @@
+#' index a list
+#' 
+#' Provided a list, and a condition, returns the logical indices into the named
+#' part of the list provided. Uses \code{subset} like non-standard evaluation
+#' so that we can define appropriate expressions.
+#' 
+#' @param list_to_query the list to run the query on
+#' @param ... the expressions that do the queries
+#' 
+#' @export
+#' @return logical "&" of all queries
+#' 
+multi_query_list <- function(list_to_query, ...){
+  queries <- list(...)
+  
+  
+}
