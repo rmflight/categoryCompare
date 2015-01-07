14a6b87a24025fc812f4672b83bffbb88d4d08b5
Robert Flight <r.flight79@gmail.com>
2015-01-07 15:42:54 -0500

    testing getting statistically sig annotations from a statistical_results object

--- tests/testthat/test_significant_results.R
+++ tests/testthat/test_significant_results.R
@@ -18,11 +18,26 @@ odds1 <- c(TRUE, TRUE, FALSE) # odds > 0
 
 pvalue_counts1 <- c(TRUE, FALSE, TRUE) # pvalues < 0.5, counts >= 1
 
-test_that("list indexing works properly",
+test_that("list indexing works properly", {
           expect_equal(pvalue1, multi_query_list(c1, pvalues < 0.05))
           expect_equal(pvalue2, multi_query_list(c1, pvalues < 0.001))
           expect_equal(counts1, multi_query_list(c1, counts > 1))
           expect_equal(counts2, multi_query_list(c1, counts < 10))
           expect_equal(odds1, multi_query_list(c1, odds > 0))
           expect_equal(pvalue_counts1, multi_query_list(c1, pvalues < 0.5, counts >= 1))
-          expect_error(multi_query_list(c1, crap)))
+          expect_error(multi_query_list(c1, crap))
+          })
+
+context("significant annotations from statistical_results")
+
+test_stat <- new("statistical_results",
+                  annotation_id = c("a1", "a2", "a3"),
+                  statistics = list(pvalues = c(a1 = 0.01, a2 = 0.5, a3 = 0.0001),
+                    counts = c(a1 = 5, a2 = 10, a3 = 1),
+                    odds = c(a1 = 20, a2 = 100, a3 = 0)))
+
+test_that("get correct significant annotations", {
+           expect_equal(c("a1", "a3"), get_significant_annotations(test_stat, pvalues < 0.05))
+           expect_equal(c("a1", "a2"), get_significant_annotations(test_stat, odds > 10))
+           expect_equal(c("a1", "a3"), get_significant_annotations(test_stat, pvalues < 0.05, counts >= 1))
+})
