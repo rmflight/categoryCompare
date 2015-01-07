89fc13d0451f92e2664f1ed2629b7fec7320b025
Robert Flight <r.flight79@gmail.com>
2015-01-07 13:58:03 -0500

    fully kitting out the multi_query_list function with combining the logicals from each query

--- R/significant_results.R
+++ R/significant_results.R
@@ -11,7 +11,25 @@
 #' @return logical "&" of all queries
 #' 
 multi_query_list <- function(list_to_query, ...){
-  queries <- as.list(substitute(list(...)))
+  queries <- as.list(substitute(list(...)))[-1L]
   
-  browser(expr = TRUE)
+  n_query <- length(queries)
+  
+  query_results <- lapply(queries, eval, list_to_query)
+  
+  # how many objects do we have in each query
+  # they should all be the same to allow merging
+  n_objects <- unique(unlist(lapply(query_results, length)))
+  
+  if (length(n_objects) != 1){
+    stop("cannot merge queries on objects of different length", call. = FALSE)
+  }
+  
+  result_logical <- rep(TRUE, n_objects)
+  
+  for (i_query in seq(1, n_query)){
+    result_logical <- result_logical & query_results[[i_query]]
+  }
+  
+  result_logical
 }
