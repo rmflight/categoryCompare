75ac8e8c579337384b39c0039c1b1872a8c91e3a
Robert Flight <r.flight79@gmail.com>
2015-01-13 09:19:37 -0500

    moving all of the extract statistics code to the combine_enrichments file, because this is what it is going to be associated with

--- R/combine_enrichments.R
+++ R/combine_enrichments.R
@@ -238,3 +238,70 @@ combined_coefficient <- function(n1, n2){
   j_coef <- jaccard_coefficient(n1, n2)
   return((0.5 * o_coef) + (0.5 * j_coef))
 }
+
+
+#' extract statistics
+#' 
+#' extract all statistics for a \linkS4class{statistical_results} object. These
+#' can then be combined into a \code{data.frame} that can be returned or used
+#' to annotate the graph of annotations.
+#' 
+#' @param in_results the \linkS4class{statistical_results} object
+#' @return data.frame
+#' @exportMethod extract_statistics
+setMethod("extract_statistics", signature = list(in_results = "statistical_results"),
+          function(in_results) .extract_statistics_statistical_results(in_results))
+
+.extract_statistics_statistical_results <- function(in_results){
+  out_data <- as.data.frame(in_results@statistics)
+  rownames(out_data) <- in_results@annotation_id
+  
+  out_data
+}
+
+#' extract statistics
+#' 
+#' extract all statistics from a \linkS4class{combined_enrichment} object and 
+#' create a \code{data.frame} where each statistic from the underlying 
+#' \linkS4class{statistical_results} object in each of the enrichments
+#' is named according to which enrichment it was in and what statistic it was.
+#' 
+#' @param in_results the \linkS4class{combined_enrichment} object
+#' @return data.frame
+#' @exportMethod extract_statistics
+setMethod("extract_statistics", signature = list(in_results = "combined_enrichment"),
+          function(in_results) .extract_statistics_combined_enrichment(in_results))
+
+.extract_statistics_combined_enrichment <- function(in_results){
+  all_data <- lapply(in_results@enriched, function(x){extract_statistics(x@statistics)})
+  
+  all_rows <- unique(unlist(lapply(all_data, rownames)))
+  
+  new_named_data <- lapply(names(all_data), function(x){
+    tmp_x <- all_data[[x]]
+    names(tmp_x) <- paste(x, names(tmp_x), sep = ".")
+    tmp_x
+  })
+  names(new_named_data) <- names(all_data)
+  
+  all_cols <- (unlist(lapply(new_named_data, colnames)))
+  
+  data_matrix <- matrix(NA, length(all_rows), length(all_cols))
+  data_frame <- as.data.frame(data_matrix)
+  
+  rownames(data_frame) <- all_rows
+  names(data_frame) <- all_cols
+  
+  for (i_data in names(new_named_data)){
+    tmp_data <- new_named_data[[i_data]]
+    data_frame[rownames(tmp_data), names(tmp_data)] <- tmp_data
+  }
+  
+  data_frame
+}
+
+#' all data extraction
+#' 
+#' given a \linkS4class{combined_enrichment} object, get the significant and present
+#' annotations, their descriptions, links (if present), and all the statistics, and 
+#' generate an object that can be used to 
