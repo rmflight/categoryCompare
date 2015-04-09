51683f8de61f37ac289d787961caec09ada74382
Robert M Flight <r.flight79@gmail.com>
2015-04-09 13:24:06 -0400

    and return the graph, not the logical matrix

--- R/combine_enrichments.R
+++ R/combine_enrichments.R
@@ -64,6 +64,7 @@ setMethod("generate_annotation_graph", signature = list(comb_enrichment = "combi
   annotation_graph <- add_data_to_graph(annotation_graph, annotation_table)
   
   annotation_graph@significant <- comb_enrichment@statistics@significant@significant[in_graph_annotation, , drop = FALSE]
+  annotation_graph
 }
 
 #' add table data to graph
